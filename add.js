471df6c32bad3fb3513e93549512ab6b585cba04
Kishore-K-SE <kishore.k@itilite.in>
2022-09-28 14:27:46 +0530

    addition feature

---
+++ add.js
@@ -0,0 +1,8 @@
+function add(){
+var num1 = document.getElementById("firstno").value
+var num2 = document.getElementById("secondno").value
+
+var result = num1+num2
+
+document.getElementById("result") = result
+}
